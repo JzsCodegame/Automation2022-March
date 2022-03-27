a5b30f04a270963d7e8d9fc5eec8a1154402d8e9
Javid Zaman <zamanjavid@gmail.com>
2022-03-27 16:33:54 +0000

    automation2022 Second Commit

--- src/PracticeClass/ClassA.java
+++ src/PracticeClass/ClassA.java
@@ -2,10 +2,10 @@ package PracticeClass;
 
 public class ClassA {
 	// Declaration of Instance Variable. 
-	 private int data = 30; // Here, instance variable is private. 
+	 public int data = 30; // Here, instance variable is private. 
 
 	// Declaration of instance method. 
-	  private void msg() // Here, method is also private. 
+	 public void msg() // Here, method is also private. 
 	  { 
 	    System.out.println("Hello Java, this is my first java program"); 
 	  } 
--- src/PracticeClass/Constructors.java
+++ src/PracticeClass/Constructors.java
@@ -13,14 +13,15 @@ public class Constructors {
 			 System.out.println("No-Argument Constructor called"); //It has an argument or code.
 			 }*/
 	
-	Constructors (String name) { 
-		this.name = name;
+	Constructors (String name) { //String name is our parameter because we have created inside the method()
+		this.name = name; //This is the name parameter which is our container.
 		}
 		 
 		 
 	
 	public static void main(String[] args) {
-		Constructors C = new Constructors("Keya");
+		Constructors C = new Constructors("Keya");/*We have instantiated our object of class which is C, 
+		and using the Constructor we are manipulating date*/
 		System.out.println(C.name);
 	}
 	
