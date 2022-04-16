c094d227109219adc7f02d2d20a2299f95bdc6ea
Javid Zaman <zamanjavid@gmail.com>
2022-04-16 14:53:35 +0000

    changes from 4/16/2022

--- src/BaseModel/BaseClass.java
+++ src/BaseModel/BaseClass.java
@@ -16,8 +16,8 @@ import org.testng.annotations.BeforeTest;
 public class BaseClass {
 
 	
-	public static WebDriver driver; //Static Object can be accessed anywhere
-    public static String AppUrl;//Static Variable can be accessed anywhere 
+	public static WebDriver driver; //We create at class level.. so Static Object can be accessed anywhere.  
+    public static String AppUrl;//We create at class level.. so Static Variable can be accessed anywhere 
     public static String Google;
 	
 	
@@ -26,49 +26,49 @@ public class BaseClass {
     
     @BeforeSuite() //Declare The Annotations
 	public void config() {  //Declare The Method
-		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");//Write The statements or code based on requirement
-		Google = "https://www.google.com/";
+		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");//Write The statements or code based on requirement. In this case using driver configurations.
+		Google = "https://www.google.com/";//Variable Configurations meaning provide values to variables.
 		AppUrl = "http://automationpractice.com/index.php";
 	}
 	
 	@BeforeTest()
 	public void setup() {
-		driver = new ChromeDriver();//ChromeDriver() is the Constructor ChromeDriver class.
+		driver = new ChromeDriver();//ChromeDriver() is the Constructor ChromeDriver class.So before test we instantiate which driver we will use.
 		
 	}
 	
 	@BeforeClass()
 	 public void Synchronize(){
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);// Implicitly wait in general for 10 seconds.
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);// Implicitly wait in general for 10 seconds. We synchronize how the code and machine will run.
 	}
 	
 	@BeforeMethod() 
 	public void BrowserConfig() {
-		driver.manage().window().maximize();
+		driver.manage().window().maximize(); //We maximize or change browser based on our preference.
 	}
     
 	@AfterMethod()
 	public void validations(){
-    	System.out.println("Validations succesfully");
+    	System.out.println("Validations succesfully");// We validate our code executions.
     	System.out.println("Method executed succesfully");
     	
     }
 	@AfterClass() 
 	  public void PageValidations() {
-		System.out.println("Page executed succesfully");
+		System.out.println("Page executed succesfully");// We print confirmation message that page has been executed successfully. 
 	}
 	
 	@AfterTest()
 	public void BrowserClosing() throws InterruptedException {
-		Thread.sleep(2000);
-		driver.close();
+		Thread.sleep(2000); //We use thread.sleep to make Java wait so we can see our execution results.
+		driver.close();// We close the browser
 		
 	}
 	
 	
 	@AfterSuite
 	public void terminate() {
-		driver.quit();
+		driver.quit(); // We close the instance and terminate the session. It will erase all the cache memory and make our next execution better.
 	}
 	
 }
